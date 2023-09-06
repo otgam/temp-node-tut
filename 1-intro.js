733a65211f9cdea2d0f0549960ba33be4f43c490
otgam <144167299+otgam@users.noreply.github.com>
2023-09-05 18:46:05 -1100

    add

---
+++ 1-intro.js
@@ -0,0 +1,8 @@
+const luckYnums = 12;
+
+if (luckYnums<10) {
+  console.log('small num');
+} else {
+  console.log('big number');
+}
+console.log('real nigga')
---
+++ 2-globals.js
@@ -0,0 +1,14 @@
+//GLOBALS - no window
+//__dirname   - path to current directory
+// __filename - filename
+//require     - function to module (CommonJs)
+//module      - info about current moudle (file)
+//process     - info about env where the program is being executed 
+
+setInterval(() => {
+  console.log('hello')
+}, 1000);
+
+
+
+//console.log(__filename)
